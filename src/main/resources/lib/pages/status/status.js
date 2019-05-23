de82d707bf9ac5ae7c1a1b2d13bb5226890b6c3a
runarmyklebust <rmy@enonic.com>
2019-05-23 16:45:07 +0200

    version 2.0.0

--- src/main/resources/lib/pages/status/status.js
+++ src/main/resources/lib/pages/status/status.js
@@ -1,9 +1,8 @@
-var mustache = require('/lib/xp/mustache');
+var mustache = require('/lib/mustache');
 var StatusView = resolve('./status.html');
 var aliveService = require('/lib/alive/alive');
 
 exports.handleGet = function (req) {
-
     var alive = aliveService.isAlive();
 
     if (!alive) {
--- src/main/resources/webapp/webapp.js
+++ src/main/resources/webapp/webapp.js
@@ -1,7 +1,6 @@
 var router = require('/lib/router')();
 var status = require('/lib/pages/status/status');
 
-
 router.get('/', status.handleGet);
 
 exports.all = function (req) {
