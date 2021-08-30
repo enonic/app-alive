4e6a21ba58a754c4858320c4f0b5ae5aba5358e0
rymsha <rymsha@yandex.ru>
2021-08-30 13:55:19 +0200

    Update to gradle 7.x

--- src/main/resources/webapp/webapp.js
+++ src/main/resources/webapp/webapp.js
@@ -1,7 +1,7 @@
 var router = require('/lib/router')();
 var status = require('/lib/pages/status/status');
 
-router.get('/', status.handleGet);
+router.get('/?', status.handleGet);
 
 exports.all = function (req) {
     return router.dispatch(req);
